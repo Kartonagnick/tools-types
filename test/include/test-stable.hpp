404a723d2a55d0182fc94df6d19c1fa2ff82d806
Kartonagnick <CastleOfDreams@yandex.ru>
2021-02-20 15:52:15 +0300

    add: test

---
+++ test/include/test-stable.hpp
@@ -0,0 +1,23 @@
+                                   
+// [2021y-02m-16d] Idrisov Denis R.
+#pragma once
+
+#ifndef dTEST_STABLE_USED_ 
+#define dTEST_STABLE_USED_ 1
+
+    #define dFORBID_ZERO_SIZE_ARRAY
+    #define INCLUDE_AUTO_GENERATED
+
+    // #define INCLUDE_STRESS_TESTS
+    #define INCLUDE_LONG_LONG_TESTS
+    #define INCLUDE_LONG_TESTS
+
+//==============================================================================
+//===== modern/classic =====================================||==================
+    #define TEST_MODERN                                     // ready!
+    #define TEST_CLASSIC                                    // ready!
+//==============================================================================
+//==============================================================================
+
+#endif // !dTEST_STABLE_USED_
+
---
+++ test/sources/main.cpp
@@ -0,0 +1,17 @@
+
+// [2021y-02m-05d] Idrisov Denis R.
+// [2021y-02m-20d] birthday of the project
+#include <mygtest/main.hpp>
+//==============================================================================
+//==============================================================================
+
+GTEST_API_ int main(int argc, char** argv)
+{
+    // example settings:
+    //   test.ext --gtest_filter=tools.stopwatch* --stress
+    //   testing::GTEST_FLAG(filter) = "tools.stopwatch_*";
+    return ::testing::run(argc, argv);
+}
+
+//==============================================================================
+//==============================================================================
