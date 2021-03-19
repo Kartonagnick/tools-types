7a4d402852c05f737c0aa78cba617637af5dd1a6
Kartonagnick <CastleOfDreams@yandex.ru>
2021-03-19 18:40:34 +0300

    update: tools/types

--- include/tools/types/types.hpp
+++ include/tools/types/types.hpp
@@ -1,12 +1,13 @@
 // [2021y-03-16d][16:55:29] Idrisov Denis R. 100
 // [2021y-03-17d][23:51:45] Idrisov Denis R. 101
+// [2021y-03-19d][18:22:39] Idrisov Denis R. 102 PRE
 #pragma once
 #ifndef dTOOLS_TYPES_USED_ 
-#define dTOOLS_TYPES_USED_ 101
+#define dTOOLS_TYPES_USED_ 102
 
 #include <tools/features.hpp>
-#include <tools/common.hpp>
-#include <tools/fixed.hpp>
+#include <tools/types/fixed.hpp>
+#include <tools/types/common.hpp>
 
 //==============================================================================
 //==============================================================================
