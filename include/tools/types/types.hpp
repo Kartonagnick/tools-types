b15d0a609955718078468b701deac0556f0b624b
Kartonagnick <CastleOfDreams@yandex.ru>
2021-03-21 04:04:50 +0300

    update: version (1.0.6) PRE

--- include/tools/types/types.hpp
+++ include/tools/types/types.hpp
@@ -4,9 +4,10 @@
 // [2021y-03m-19d][22:55:07] Idrisov Denis R. 103
 // [2021y-03m-20d][05:33:09] Idrisov Denis R. 104
 // [2021y-03m-20d][19:42:05] Idrisov Denis R. 105
+// [2021y-03m-21d][03:69:30] Idrisov Denis R. 106 PRE
 #pragma once
 #ifndef dTOOLS_TYPES_USED_ 
-#define dTOOLS_TYPES_USED_ 105
+#define dTOOLS_TYPES_USED_ 106 PRE
 
 #include <tools/features.hpp>
 #include <tools/types/fixed.hpp>
--- include/tools/types/version.hpp
+++ include/tools/types/version.hpp
@@ -3,13 +3,14 @@
 // [2021y-03m-19d][22:54:18] Idrisov Denis R. 103
 // [2021y-03m-20d][05:33:17] Idrisov Denis R. 104
 // [2021y-03m-20d][19:41:45] Idrisov Denis R. 105
+// [2021y-03m-21d][03:59:55] Idrisov Denis R. 106 PRE
 #pragma once
 #ifndef dTOOLS_TYPES_VERSION_USED_ 
-#define dTOOLS_TYPES_VERSION_USED_ 105
+#define dTOOLS_TYPES_VERSION_USED_ 106
 
 #define dTYPES_MAJOR 1
 #define dTYPES_MINOR 0
-#define dTYPES_PATCH 5
+#define dTYPES_PATCH 6 PRE
 
 //==============================================================================
 //==============================================================================
