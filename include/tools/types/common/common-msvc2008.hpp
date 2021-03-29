53e5441fa4d224cdd9b86e0f078c1146871e33eb
Kartonagnick <CastleOfDreams@yandex.ru>
2021-03-29 23:44:33 +0300

    update: common (106) PRE

--- include/tools/types/common/common-msvc2008.hpp
+++ include/tools/types/common/common-msvc2008.hpp
@@ -2,9 +2,10 @@
 // [2021y-03m-20d][19:43:08] Idrisov Denis R. 103
 // [2021y-03m-21d][17:51:39] Idrisov Denis R. 104
 // [2021y-03m-23d][23:55:08] Idrisov Denis R. 105
+// [2021y-03m-29d][23:43:28] Idrisov Denis R. 106 PRE
 #pragma once
 #ifndef dTOOLS_COMMON_NEW_USED_ 
-#define dTOOLS_COMMON_NEW_USED_ 105,2008
+#define dTOOLS_COMMON_NEW_USED_ 106,2008
 
 #include <cstddef>
 
@@ -411,14 +412,25 @@ namespace tools
     dPRAGMA_PUSH_WARNING_QUALIFIER_RETURN_TYPE
 
     template<class t> struct add_const_data
-        { typedef const t type; };
+    {
+        typedef const t type; 
+    };
 
     template<class t> struct add_const_data<t&>
-        { typedef typename add_const_data<t>::type& type; };
+    {
+        typedef typename add_const_data<t>::type 
+            x;
+        typedef x& type; 
+    };
 
     #ifdef dHAS_RVALUE_REFERENCES
     template<class t> struct add_const_data<t&&>
-        { typedef typename add_const_data<t>::type&& type; };
+    {
+        typedef typename add_const_data<t>::type 
+            x;
+        typedef x&& type; 
+        //typedef typename add_const_data<t>::type&& type; 
+    };
     #endif
 
     template<class t> struct add_const_data<t[]>
@@ -429,38 +441,74 @@ namespace tools
     };
 
     template<class t, size_t n> struct add_const_data<t[n]>
-        { typedef typename add_const_data<t>::type type[n]; };
+    {
+        typedef typename add_const_data<t>::type 
+            x;
+        typedef x type[n];
+    };
 
     #ifdef dHAS_ZERO_SIZE_ARRAY
     dPRAGMA_PUSH_WARNING_ZERO_SIZE_ARRAY
     template<class t> struct add_const_data<t[0]>
-        { typedef typename add_const_data<t>::type[0]; };
+    {
+        typedef typename add_const_data<t>::type 
+            x;
+        typedef x type[0];
+        // typedef typename add_const_data<t>::type[0]; 
+    };
     dPRAGMA_POP
     #endif // !dHAS_ZERO_SIZE_ARRAY
 
     template<class t> struct add_const_data<t*>
-        { typedef typename add_const_data<t>::type* type; };
+    {
+        typedef typename add_const_data<t>::type
+            x;
+        typedef x* type;
+    };
 
     template<class t> struct add_const_data<t* const>
-        { typedef typename add_const_data<t>::type*const type; };
+    {
+        typedef typename add_const_data<t>::type
+            x;
+        typedef x* const type;
+        //typedef typename add_const_data<t>::type*const type; 
+    };
 
     template<class t> struct add_const_data<t* volatile>
-        { typedef typename add_const_data<t>::type*volatile type; };
+    {
+        typedef typename add_const_data<t>::type
+            x;
+        typedef x* volatile type;
+        //typedef typename add_const_data<t>::type*volatile type;
+    };
 
     template<class t> struct add_const_data<t* volatile const>
-        { typedef typename add_const_data<t>::type*volatile const type; };
+    {
+        typedef typename add_const_data<t>::type
+            x;
+        typedef x* volatile const type;
+        //typedef typename add_const_data<t>::type*volatile const type;
+    };
 
     template<class m, class cl> struct add_const_data<m cl::*>
-        { typedef const m cl::*type; };
+    { 
+        typedef const m cl::*type;
+    };
 
     template<class m, class cl> struct add_const_data<m cl::*const>
-        { typedef const m cl::*const type; };
+    { 
+        typedef const m cl::*const type;
+    };
 
     template<class m, class cl> struct add_const_data<m cl::*volatile>
-        { typedef const m cl::*volatile type; };
+    { 
+        typedef const m cl::*volatile type; 
+    };
 
     template<class m, class cl> struct add_const_data<m cl::*volatile const>
-        { typedef const m cl::*volatile const type; };
+    {
+        typedef const m cl::*volatile const type; 
+    };
 
     dPRAGMA_POP
 
