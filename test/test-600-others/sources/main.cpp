44e762ab26adf7766f5c7fccc2375c536741be68
Kartonagnick <CastleOfDreams@yandex.ru>
2021-03-31 22:33:41 +0300

    add: test-600-others

---
+++ test/test-600-others/sources/main.cpp
@@ -0,0 +1,16 @@
+// [2021y-02m-05d][00:01:43] Idrisov Denis R.
+// [2021y-02m-20d][00:01:43] birthday of the project
+#include <mygtest/main.hpp>
+//==============================================================================
+//==============================================================================
+
+GTEST_API_ int main(int argc, char** argv)
+{
+    // example settings:
+    //   test.ext --gtest_filter=tools.stopwatch* --stress
+    //   testing::GTEST_FLAG(filter) = "tools.stopwatch_*";
+    return ::testing::run(argc, argv);
+}
+
+//==============================================================================
+//==============================================================================
