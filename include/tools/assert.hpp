6768e02fafe8fd48127dd67fe6d595cc40cb8ced
Kartonagnick <CastleOfDreams@yandex.ru>
2021-03-31 22:32:59 +0300

    bug: assert (101) PRE

--- include/tools/assert.hpp
+++ include/tools/assert.hpp
@@ -1,12 +1,13 @@
 // [2021y-02m-07d][18:40:18] Idrisov Denis R.
+// [2021y-03m-31d][22:19:54] Idrisov Denis R. 101 PRE
 #pragma once
 #ifndef dTOOLS_ASSERT_USED_
-#define dTOOLS_ASSERT_USED_ 100
+#define dTOOLS_ASSERT_USED_ 101 PRE
 //==============================================================================
 //==============================================================================
 
 #ifdef NDEBUG
-    #define dASSERT(...)
+    #define dASSERT(...) void()
 #else
     #include <cassert>
     #define dASSERT(...) assert((__VA_ARGS__))
