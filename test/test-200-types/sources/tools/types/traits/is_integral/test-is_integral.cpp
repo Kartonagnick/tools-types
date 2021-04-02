89c5ef8b4e6719a90a27170060b243c6e4312c9b
Kartonagnick <CastleOfDreams@yandex.ru>
2021-04-02 23:52:49 +0300

    update: is_integral

--- test/test-200-types/sources/tools/types/traits/is_integral/test-is_integral.cpp
+++ test/test-200-types/sources/tools/types/traits/is_integral/test-is_integral.cpp
@@ -35,24 +35,32 @@ namespace
 //==============================================================================
 TEST_COMPONENT(000)
 {
-    dCHECK(wchar_t  , false);
+    dCHECK(wchar_t  , true);
 
 #if !defined(_MSC_VER) || _MSC_VER >= 1600
     dCHECK(char16_t          , true );
     dCHECK(char32_t          , true );
 #endif
+
+#ifdef __cpp_char8_t
+    dCHECK(char8_t           , true );
+#endif 
+
     dCHECK(short             , true );
-    dCHECK(unsigned short    , true );
     dCHECK(int               , true );
-    dCHECK(unsigned          , true );
     dCHECK(long              , true );
+    dCHECK(long long         , true );
+
+    dCHECK(unsigned short    , true );
+    dCHECK(unsigned int      , true );
     dCHECK(unsigned long     , true );
     dCHECK(unsigned long long, true );
-    dCHECK(size_t            , true );
     dCHECK(unsigned          , true );
+    dCHECK(size_t            , true );
 
-    dCHECK(signed char       , false);
-    dCHECK(unsigned char     , false);
+
+    dCHECK(signed char       , true );
+    dCHECK(unsigned char     , true );
     dCHECK(float             , false);
     dCHECK(double            , false);
     dCHECK(long double       , false);
