7b350cc1126c7266f3ad4d3021ce319b83884927
Kartonagnick <CastleOfDreams@yandex.ru>
2021-04-06 23:21:38 +0300

    add: 2013

---
+++ include/tools/types/common/2013/degradate-2013.hpp
@@ -0,0 +1,19 @@
+// [2021y-04m-06d][16:24:22] Idrisov Denis R. 100
+#pragma once
+#ifndef dTOOLS_DEGRADATE_USED_ 
+#define dTOOLS_DEGRADATE_USED_ 100, 2013
+
+#include <type_traits>
+
+namespace tools 
+{
+    template<class t> 
+    using degradate = ::std::remove_cv_t<
+        ::std::remove_reference_t<t> 
+    >;
+
+} // namespace tools 
+
+//==============================================================================
+//==============================================================================
+#endif // !dTOOLS_COMMON_USED_
---
+++ include/tools/types/common/2013/rvalue-2013.hpp
@@ -0,0 +1,33 @@
+// [2021y-04m-06d][16:33:07] Idrisov Denis R. 100
+#pragma once
+#ifndef dTOOLS_FOR_RVALUE_USED_ 
+#define dTOOLS_FOR_RVALUE_USED_ 100, 2013
+
+#include <type_traits>
+
+//==============================================================================
+//==============================================================================
+
+namespace tools
+{
+    #define dfor_rvalue(t) ::tools::for_rvalue<t>* = nullptr
+    template<class t, class ret = void> 
+        using for_rvalue
+            = ::std::enable_if_t<
+                ::std::is_rvalue_reference<t>::value,
+                ret
+            >;
+
+    #define dfor_not_rvalue(t) ::tools::for_not_rvalue<t>* = nullptr
+    template<class t, class ret = void> 
+        using for_not_rvalue
+            = ::std::enable_if_t<
+                ! ::std::is_rvalue_reference<t>::value,
+                ret
+            >;
+
+} // namespace tools
+
+//==============================================================================
+//==============================================================================
+#endif // !dTOOLS_FOR_RVALUE_USED_
