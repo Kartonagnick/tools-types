7a7370b3c08e28fa616f057ebbf581b571506631
Kartonagnick <CastleOfDreams@yandex.ru>
2021-04-06 23:23:01 +0300

    add: find_types

---
+++ include/tools/types/common/find_types/find_types-2013.hpp
@@ -0,0 +1,52 @@
+// [2021y-03m-29d][23:43:28] Idrisov Denis R. 100
+#pragma once
+#ifndef dTOOLS_FIND_TYPES_2013_USED_ 
+#define dTOOLS_FIND_TYPES_2013_USED_ 100,2013
+
+#include <type_traits>
+
+#define dDETAIL_CONSTANT(...)              \
+    public dTRAIT::integral_constant<bool, \
+        detail::__VA_ARGS__::value         \
+    >
+
+//==============================================================================
+//=== find_type ================================================================
+#ifndef dTOOLS_FIND_TYPE_USED_ 
+#define dTOOLS_FIND_TYPE_USED_ 100,2013
+namespace tools
+{
+    // if type 't' is in the list 'args' --> true
+    namespace detail
+    {
+        template<class t, class ...args>
+            struct find_type_;
+
+        template<class t, class s, class ...args>
+        struct find_type_<t, s, args...>
+        {
+            using x = ::tools::detail::find_type_<t, args...>;
+            enum { cur = ::std::is_same<t, s>::value };
+            enum { value = cur || x::value };
+        };
+
+        template<class t> struct find_type_<t>
+        {
+            enum { value = false };
+        };
+
+    } // namespace detail
+
+    // if type 't' is in the list 'args' --> true
+    template<class t, class ...args>
+    struct find_type
+        : dDETAIL_CONSTANT(find_type_<t, args...>)
+    {};
+
+} // namespace tools 
+#endif // !dTOOLS_FIND_TYPE_USED_
+
+
+//==============================================================================
+//==============================================================================
+#endif // !dTOOLS_FIND_TYPES_2013_USED_
