635ae42be445e859281e5a420fb2632a190f8184
Kartonagnick <CastleOfDreams@yandex.ru>
2021-04-09 23:44:02 +0300

    update: is_volatile_data

--- include/tools/types/common/is_volatile_data/is_volatile_data-2013.hpp
+++ include/tools/types/common/is_volatile_data/is_volatile_data-2013.hpp
@@ -12,13 +12,35 @@ namespace tools
 {
     namespace detail
     {
+        template<class t, bool> struct is_volatile_data_impl_
+        {
+        private:
+            using y = decltype(*::std::declval<t>());
+            using x = ::std::remove_reference_t<y>;
+            //using z = ::std::remove_pointer_t<x>;
+        public:
+            enum { value = ::std::is_volatile<x>::value };
+        };
+
+        template<class t> 
+        struct is_volatile_data_impl_<t, false>
+            { enum { value = false }; };
+
         template<class t> class is_volatile_data_
         {
             using x = ::std::remove_reference_t<t>;
             using z = ::std::remove_pointer_t<x>;
-            using v = ::std::is_volatile<z>;
+            using d = ::tools::is_dereferencable<x>;
+            using c = ::std::is_volatile<z>;
+            
+            enum { ok = d::value };
+            using h
+                = ::tools::detail::is_volatile_data_impl_<x, ok>;
+
+            enum { top = !ok && c::value };
+            enum { dwn =  ok && h::value };
         public:
-            enum { value = v::value };
+            enum { value = top || dwn };
         };
 
     } // namespace detail
@@ -26,6 +48,26 @@ namespace tools
     template<class t> struct is_volatile_data
         : dDETAIL_CONSTANT(is_volatile_data_<t>)
     {};
+
+    template<class t, class v = void> 
+    using for_volatile_data 
+        = ::std::enable_if_t< ::tools::is_volatile_data<t>::value, v>;
+
+    template<class t, class v = void> 
+    using for_not_volatile_data 
+        = ::std::enable_if_t< !::tools::is_volatile_data<t>::value, v>;
+
+    #define dif_volatile_data(s, r) \
+        ::tools::for_volatile_data<s, r>
+
+    #define dif_not_volatile_data(s, r) \
+        ::tools::for_not_volatile_data<s, r>
+
+    #define dfor_volatile_data(s) \
+        dif_volatile_data(s, void)* = nullptr
+
+    #define dfor_not_volatile_data(s) \
+        dif_not_volatile_data(s, void)* = nullptr
 
 } // namespace tools
 
