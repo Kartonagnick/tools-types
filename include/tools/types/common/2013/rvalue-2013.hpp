3dd273a51af01da635ca2ed94ef96c2e1e1489ac
Kartonagnick <CastleOfDreams@yandex.ru>
2021-04-10 04:36:09 +0300

    update: rvalue (101) PRE

--- include/tools/types/common/2013/rvalue-2013.hpp
+++ include/tools/types/common/2013/rvalue-2013.hpp
@@ -1,7 +1,8 @@
 // [2021y-04m-06d][16:33:07] Idrisov Denis R. 100
+// [2021y-04m-10d][03:37:41] Idrisov Denis R. 101
 #pragma once
 #ifndef dTOOLS_FOR_RVALUE_USED_ 
-#define dTOOLS_FOR_RVALUE_USED_ 100,2013
+#define dTOOLS_FOR_RVALUE_USED_ 101,2013
 
 #include <type_traits>
 
@@ -10,17 +11,17 @@
 
 namespace tools
 {
-    #define dfor_rvalue(t) ::tools::for_rvalue<t>* = nullptr
+    #define dfor_rvalue(t) ::tools::for_rvalue_t<t>* = nullptr
     template<class t, class ret = void> 
-        using for_rvalue
+        using for_rvalue_t
             = ::std::enable_if_t<
                 ::std::is_rvalue_reference<t>::value,
                 ret
             >;
 
-    #define dfor_not_rvalue(t) ::tools::for_not_rvalue<t>* = nullptr
+    #define dfor_not_rvalue(t) ::tools::for_not_rvalue_t<t>* = nullptr
     template<class t, class ret = void> 
-        using for_not_rvalue
+        using for_not_rvalue_t
             = ::std::enable_if_t<
                 ! ::std::is_rvalue_reference<t>::value,
                 ret
