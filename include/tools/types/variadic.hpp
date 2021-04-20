3f749f8df6ace1ad18ee5dff34deb0968eba1693
Kartonagnick <CastleOfDreams@yandex.ru>
2021-04-20 23:22:39 +0300

    bugfix: variadic(003) PRE

--- include/tools/types/variadic.hpp
+++ include/tools/types/variadic.hpp
@@ -2,10 +2,11 @@
 // [2020y-02m-20d][18:40:18] Idrisov Denis R.
 // [2021y-03m-25d][23:21:33] Idrisov Denis R.
 // [2021y-03m-26d][08:27:10] Idrisov Denis R. 002
+// [2021y-04m-20d][23:07:59] Idrisov Denis R. 003 PRE
 //==============================================================================
 #pragma once
 #ifndef dTOOLS_VARIADIC_USED_ 
-#define dTOOLS_VARIADIC_USED_ 2
+#define dTOOLS_VARIADIC_USED_ 3 PRE
 
 #include <tools/features.hpp>
 
